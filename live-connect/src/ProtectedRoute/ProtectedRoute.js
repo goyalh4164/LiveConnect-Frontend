91c7b45a4532200a5a0d8fb44d117fddc6545253
Harsh <goyalh4164@gmail.com>
2024-02-01 23:43:56 +0530

    minor change

--- live-connect/src/ProtectedRoute/ProtectedRoute.js
+++ live-connect/src/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,7 @@
 // ProtectedRoute.js
 
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuth } from '../Context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
