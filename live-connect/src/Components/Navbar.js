5132c691afce8f2bcf4b54229135774eff130453
Harsh <goyalh4164@gmail.com>
2024-02-01 18:36:32 +0530

    added Home Link to the logo

--- live-connect/src/Components/Navbar.js
+++ live-connect/src/Components/Navbar.js
@@ -1,16 +1,26 @@
 // components/Navbar.js
 
 import React from 'react';
-import { Box, Flex, Heading, Spacer, Link as ChakraLink, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Spacer,
+  Link as ChakraLink,
+  Button,
+} from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <Box bg="teal.500" p={4} color="white">
       <Flex align="center">
-        <Heading as="h2" size="md">
-          LiveConnect
-        </Heading>
+        <ChakraLink as={RouterLink} to="/" mr={4}>
+          <Heading as="h2" size="md">
+            LiveConnect
+          </Heading>
+        </ChakraLink>
+
         <Spacer />
         <Flex>
           <ChakraLink as={RouterLink} to="/login" mr={4}>
